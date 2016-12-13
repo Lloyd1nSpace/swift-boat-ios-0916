829c53f15262df899fe26008a8a7f92161547874
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-12-13 10:56:30 -0500

    Getting started up

---
+++ swift-boat/Boat.swift
@@ -0,0 +1,13 @@
+//
+//  Boat.swift
+//  swift-boat
+//
+//  Created by Lloyd W. Sykes on 12/13/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class Boat {
+    
+}
