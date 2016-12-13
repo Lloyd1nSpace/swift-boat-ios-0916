0ba21d5ac35ed5536354b6a2a2576be2bee14257
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-12-13 11:02:53 -0500

    Tests pass lab succeeded

--- swift-boat/Boat.swift
+++ swift-boat/Boat.swift
@@ -10,4 +10,20 @@ import Foundation
 
 class Boat {
     
+    let name: String
+    var sailors: [String]
+    var maxSpeedKnots: Double
+    var speedKnots: Double = 0
+    
+    init(name: String, sailors: [String], maxSpeedKnots: Double) {
+        self.name = name
+        self.sailors = sailors
+        self.maxSpeedKnots = maxSpeedKnots
+    
+    }
+    
+    convenience init(name: String, maxSpeedKnots: Double) {
+        self.init(name: name, sailors: [], maxSpeedKnots: maxSpeedKnots)
+    }
+    
 }
